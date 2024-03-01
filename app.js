60f4b127da94da3fe1b6c6565f60a118c46f2b2d
Louisa <mccolganlouisa@gmail.com>
2024-03-01 13:55:54 +0000

    Inches to centimeters conversion

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-/*function waist() { 
-    const form = document.getElementById('circleForm');
-    const waist = 16;
-    const length = form.elements.length.value;
-    const seam = form.elements.seam.value;
-
-    console.log(waist);
-    document.getElementById("circleOutput").textContent = waist;
- // Prevent the default form submission behavior
- return false;
-
- };
-
- event.preventDefault(); */
 
  function calcRadius() {
 
@@ -31,5 +17,48 @@
     return false;
 }
 
-//Convert the decimal to a fraction of an inch
+function setRuleValue(selector, prop, value) {
+    selector = selector.toLowerCase();
+
+    var sheet, sheets = document.styleSheets;
+    var j, rule, rules;
+
+    if (sheets) {
+
+          // Search style sheets for rule
+    for (var i=0, iLen=sheets.length; i<iLen; i++) {
+        sheet = sheets[i];
+        j = 0;
+  
+        // Choose .cssRules or .rules, whichever is supported
+        while (rule = sheet.cssRules[j]) {
+          j++;
+  
+          if (rule.selectorText.toLowerCase() == selector) {
+            rule.style[prop] = value;
+  
+            // If had success, return true
+            return true;
+          }
+        }
+      }
+    }
+    // If get here, didn't find rule so return false
+    return false;
+  }
+  
+  // Turn metric on, imperial off
+  function showInches() {
+    setRuleValue('.inches','display', '');
+    setRuleValue('.centimeters', 'display', 'none' );
+  }
+  
+  // Turn imperial on, metric off
+  function showCentimeters() {
+    setRuleValue('.inches','display', 'none');
+    setRuleValue('.centimeters', 'display', '' );
+  }
+  
+
+//Convert the decimal to a fraction of an inch (Use Span)
 //offer a cm version
